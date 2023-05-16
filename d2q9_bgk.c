17a48ff8a34c8ff7fb113f02f04bb7baa081a58d
Letong Han <hanlt@shanghaitech.edu.cn>
2023-05-16 09:15:11 +0800

    (128, 256)

--- d2q9_bgk.c
+++ d2q9_bgk.c
@@ -38,7 +38,7 @@ int timestep(const t_param params, t_speed *cells, t_speed *tmp_cells,
 ** the local equilibrium distribution and relaxation process
 */
 
-#define chunk_x 128
+#define chunk_x 256
 #define chunk_y 128
 inline int fuse(int start_col, int end_col, const t_param params,
                 t_speed *cells, t_speed *tmp_cells, int *obstacles) {
