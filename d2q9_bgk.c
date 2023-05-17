951c2c3d02a194b36605862db94d70fb14c5dda8
Letong Han <hanlt@shanghaitech.edu.cn>
2023-05-17 19:33:38 +0800

    128

--- d2q9_bgk.c
+++ d2q9_bgk.c
@@ -52,7 +52,7 @@ int timestep_end(const t_param params, t_speed *cells, t_speed *tmp_cells,
 ** the local equilibrium distribution and relaxation process
 */
 
-#define chunk_x 64
+#define chunk_x 128
 #define chunk_y 64
 
 int collision(int start_col, int end_col, const t_param params, t_speed *cells,
